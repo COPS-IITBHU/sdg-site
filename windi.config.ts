2caa9e34b9e816df644750af56ec7858569ae7fd
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-09-11 23:50:51 +0530

    fix prettier command to work only on code files
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- windi.config.ts
+++ windi.config.ts
@@ -1,7 +1,7 @@
-import typo from 'windicss/plugin/typography'
+import typo from 'windicss/plugin/typography';
 
 export default {
   darkMode: 'class',
   plugins: [typo()],
   safelist: 'prose prose-sm m-auto text-left',
-}
+};
