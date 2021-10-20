968e1c160fb974a4477b644c571bae38de92a7ed
Sanskar Santosh Totla <sanskartotla22@gmail.com>
2021-10-20 12:37:06 +0530

    functional achievements carousel Co-authored-by: Sanskar Santosh Totla <sanskartotla22@gmail.com> Co-authored-by: P Aswini Kumar <aswini123p@gmail.com>

--- components/achievementsCarousel.vue
+++ components/achievementsCarousel.vue
@@ -1,136 +1,133 @@
 <template>
   <body>
     <div class="gallery">
-      <div ref="galleryContainer" class="gallery-container">
-        <li
-          ref="galleryItem"
-          class="box gallery-item gallery-item-1"
-          data-index="1"
-        >
-          Achievement 1
-        </li>
-        <li
-          ref="galleryItem"
-          class="box gallery-item gallery-item-2"
-          data-index="2"
-        >
-          Achievement 2
-        </li>
-        <li
-          ref="galleryItem"
-          class="box gallery-item gallery-item-3"
-          data-index="3"
-        >
-          Achievement 3
-        </li>
-        <li
-          ref="galleryItem"
-          class="box gallery-item gallery-item-4"
-          data-index="4"
-        >
-          Achievement 4
-        </li>
-        <li
-          ref="galleryItem"
-          class="box gallery-item gallery-item-5"
-          data-index="5"
-        >
-          Achievement 5
-        </li>
+      <div class="gallery-container">
+        <div class="box gallery-item gallery-item-1 bg-gray-100" data-index="1">
+          <h1>Achievement 1</h1>
+          <h3>bla bla bla</h3>
+        </div>
+        <div class="box gallery-item gallery-item-2 bg-gray-100" data-index="2">
+          <h1>Achievement 2</h1>
+          <h3>bla bla bla</h3>
+        </div>
+        <div class="box gallery-item gallery-item-3 bg-gray-100" data-index="3">
+          <h1>Achievement 3</h1>
+          <h3>bla bla bla</h3>
+        </div>
+        <div class="box gallery-item gallery-item-4 bg-gray-100" data-index="4">
+          <h1>Achievement 4</h1>
+          <h3>bla bla bla</h3>
+        </div>
+        <div class="box gallery-item gallery-item-5 bg-gray-100" data-index="5">
+          <h1>Achievement 5</h1>
+          <h3>bla bla bla</h3>
+        </div>
+        <div class="box gallery-item gallery-item-6 bg-gray-100" data-index="6">
+          <h1>Achievement 6</h1>
+          <h3>bla bla bla</h3>
+        </div>
+        <div class="box gallery-item gallery-item-7 bg-gray-100" data-index="7">
+          <h1>Achievement 7</h1>
+          <h3>bla bla bla</h3>
+        </div>
       </div>
-      <div class="gallery-controls">
-        <button @click="left"><i class="arrow left"></i></button>
-        <button @click="right"><i class="arrow right"></i></button>
-      </div>
+      <button id="prev" class="arrow left gallery-controls-previous"></button>
+      <button id="next" class="arrow right gallery-controls-next"></button>
+      <div class="gallery-controls"></div>
     </div>
   </body>
 </template>
 
 <script>
-import { defineComponent } from '@nuxtjs/composition-api';
+export default {
+  mounted() {
+    const galleryContainer = document.querySelector('.gallery-container');
+    const galleryControlsContainer =
+      document.querySelector('.gallery-controls');
+    const galleryControls = ['previous', 'next'];
+    const galleryItems = document.querySelectorAll('.gallery-item');
 
-export default defineComponent({
-  data() {
-    return {
-      carouselContainer: this.$refs.galleryContainer,
-      carouselControls: ['previous', 'add', 'next'],
-      carouselArray: this.$refs.galleryItem,
-    };
-  },
-  methods: {
-    updateGallery() {
-      this.carouselArray.forEach((el) => {
-        el.classList.remove('gallery-item-1');
-        el.classList.remove('gallery-item-2');
-        el.classList.remove('gallery-item-3');
-        el.classList.remove('gallery-item-4');
-        el.classList.remove('gallery-item-5');
-      });
-
-      this.carouselArray.slice(0, 5).forEach((el, i) => {
-        el.classList.add(`gallery-item-${i + 1}`);
-      });
-    },
-    setCurrentState(direction) {
-      if (direction.className == 'gallery-controls-previous') {
-        this.carouselArray.unshift(this.carouselArray.pop());
-      } else {
-        this.carouselArray.push(this.carouselArray.shift());
+    class Carousel {
+      constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
       }
 
-      this.updateGallery();
-    },
-    setControls() {
-      this.carouselControls.forEach((control) => {
-        galleryControlsContainer.appendChild(
-          document.createElement('button')
-        ).className = `gallery-controls-${control}`;
-
-        document.querySelector(`.gallery-controls-${control}`).innerText =
-          control;
-      });
-    },
-    useControls() {
-      const triggers = [...galleryControlsContainer.childNodes];
-
-      triggers.forEach((control) => {
-        control.addEventListener('click', (e) => {
-          e.preventDefault();
-
-          if (control.className == 'gallery-controls-add') {
-            const newItem = document.createElement('img');
-            const latestItem = this.carouselArray.length;
-            const latestIndex =
-              this.carouselArray.findIndex(
-                (item) =>
-                  item.getAttribute('data-index') == this.carouselArray.length
-              ) + 1;
-
-            // Assign the necessary properties for new gallery item
-            Object.assign(newItem, {
-              className: 'gallery-item',
-              src: `http://fakeimg.pl/300/?text=${
-                this.carouselArray.length + 1
-              }`,
-            });
-            newItem.setAttribute('data-index', this.carouselArray.length + 1);
-
-            // Then add it to the carouselArray and update the gallery
-            this.carouselArray.splice(latestIndex, 0, newItem);
-            document
-              .querySelector(`[data-index="${latestItem}"]`)
-              .after(newItem);
-            this.updateGallery();
-          } else {
-            this.setCurrentState(control);
-          }
+      // Update css classes for gallery
+      updateGallery() {
+        this.carouselArray.forEach((el) => {
+          el.classList.remove('gallery-item-1');
+          el.classList.remove('gallery-item-2');
+          el.classList.remove('gallery-item-3');
+          el.classList.remove('gallery-item-4');
+          el.classList.remove('gallery-item-5');
         });
-      });
-    },
 
-    async getName() {},
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+          el.classList.add(`gallery-item-${i + 1}`);
+        });
+      }
+
+      // Update the current order of the carouselArray and gallery
+      setCurrentState(direction) {
+        if (direction.className === 'arrow left gallery-controls-previous') {
+          this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+          this.carouselArray.push(this.carouselArray.shift());
+        }
+
+        this.updateGallery();
+      }
+
+      // setControls() {
+      // this.carouselControls.forEach((control) => {
+      //   galleryControlsContainer.appendChild(
+      //     document.createElement('button')
+      //   ).className = `gallery-controls-${control}`;
+      //   document.querySelector(`.gallery-controls-${control}`).innerText =
+      //     control;
+      // });
+      // galleryControlsContainer.appendChild(
+      //   document.createElement('button')
+      // ).className = 'arrow left gallery-controls-previous';
+      // galleryControlsContainer.appendChild(
+      //   document.createElement('button')
+      // ).className = 'arrow right gallery-controls-next';
+      // }
+
+      // Add a click event listener to trigger setCurrentState method to rearrange carousel
+      useControls() {
+        // const triggers = [...galleryControlsContainer.childNodes];
+
+        document.getElementById('prev').addEventListener('click', (e) => {
+          e.preventDefault();
+          this.setCurrentState(document.getElementById('prev'));
+        });
+        document.getElementById('next').addEventListener('click', (e) => {
+          e.preventDefault();
+          this.setCurrentState(document.getElementById('next'));
+        });
+
+        // triggers.forEach((control) => {
+        //   control.addEventListener('click', (e) => {
+        //     e.preventDefault();
+        //     this.setCurrentState(control);
+        //   });
+        // });
+      }
+    }
+
+    const exampleCarousel = new Carousel(
+      galleryContainer,
+      galleryItems,
+      galleryControls
+    );
+
+    // exampleCarousel.setControls();
+    exampleCarousel.useControls();
   },
-});
+};
 </script>
 
 <style scoped>
@@ -144,17 +141,6 @@ export default defineComponent({
   height: 400px;
   margin: 0 auto;
   max-width: 1000px;
-  position: relative;
-}
-
-.box {
-  width: 12rem;
-  height: 20rem;
-  box-shadow: 0px 0px 2rem 0px #888888;
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   position: relative;
 }
 
@@ -210,26 +196,10 @@ export default defineComponent({
 }
 
 .gallery-controls {
+  color: white;
   display: flex;
   justify-content: center;
   margin: 30px 0;
-}
-
-.arrow {
-  border: solid black;
-  border-width: 0 3px 3px 0;
-  display: inline-block;
-  padding: 3px;
-}
-
-.right {
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-}
-
-.left {
-  transform: rotate(135deg);
-  -webkit-transform: rotate(135deg);
 }
 
 .gallery-controls button {
@@ -247,22 +217,33 @@ export default defineComponent({
 }
 
 .gallery-controls-previous {
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 15px;
+  margin-right: 3vw;
+  margin-left: 3vw;
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
   position: relative;
 }
 
 .gallery-controls-previous::before {
-  border: solid #000;
-  border-width: 0 2px 2px 0;
-  content: '';
+  border: solid white;
+  border-width: 0 3px 3px 0;
   display: inline-block;
-  height: 4px;
+  padding: 15px;
+  margin-right: 3vw;
+  margin-left: 3vw;
+  transform: rotate(135deg) translateY(-50%);
+  transition: left 0.15s ease-in-out;
   left: -10px;
-  padding: 2px;
+  /* left: -10px;
   position: absolute;
   top: 0;
   transform: rotate(135deg) translateY(-50%);
   transition: left 0.15s ease-in-out;
-  width: 4px;
+  width: 4px; */
 }
 
 .gallery-controls-previous:hover::before {
@@ -270,27 +251,48 @@ export default defineComponent({
 }
 
 .gallery-controls-next {
-  position: relative;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 15px;
+  margin-right: 3vw;
+  margin-left: 3vw;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
 }
 
 .gallery-controls-next::before {
-  border: solid #000;
-  border-width: 0 2px 2px 0;
-  content: '';
+  border: solid white;
+  border-width: 0 3px 3px 0;
   display: inline-block;
-  height: 4px;
-  padding: 2px;
-  position: absolute;
-  right: -10px;
-  top: 50%;
-  transform: rotate(-45deg) translateY(-50%);
-  transition: right 0.15s ease-in-out;
-  width: 4px;
+  padding: 15px;
+  margin-right: 3vw;
+  margin-left: 3vw;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
 }
 
 .gallery-controls-next:hover::before {
   right: -18px;
 }
+/* .arrow {
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 15px;
+  margin-right: 3vw;
+  margin-left: 3vw;
+}
+
+.right {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+.left {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+} */
 
 .gallery-nav {
   bottom: -15px;
