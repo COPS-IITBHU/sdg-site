5e44990b61ccce24186ffd34a68ca5866fa44110
Debarati Bandopadhyay <d.bandopadhyay.cd.cse20@itbhu.ac.in>
2021-10-23 01:44:22 +0530

    Added Contact-Us page.
    Co-authored-by: MOHIT KUMAR SHARMA <mohitkumar.sharma.ece20@itbhu.ac.in>

---
+++ pages/contact/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="text-center">
+    <div class="title"></div>
+    <br />
+    <contact />
+    <socialicon />
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+main {
+  background: black;
+}
+</style>
