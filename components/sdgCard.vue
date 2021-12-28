007aeb51b4ea801b143d00995ecf6f6dec24227d
somesh202 <77766761+somesh202@users.noreply.github.com>
2021-12-28 13:25:41 +0530

    Improved the portfolio page

--- components/sdgCard.vue
+++ components/sdgCard.vue
@@ -1,28 +1,39 @@
 <template>
-  <section class="col-md-3 col-sm-6 m-auto w-400">
-    <div class="our-team w-70 float-left relative p-10">
+  <section class="">
+    <div
+      class="
+        our-team
+        w-60
+        float-left
+        m-auto
+        text-center
+        relative
+        overflow-hidden
+      "
+    >
+      <img :src="imageUrl" alt="profile" class="w-100% h-auto" />
       <div
         class="
-          pic
-          rounded-l-3xl
-          overflow-hidden
-          border-t-5 border-b-5 border-gray-600
-          hover:border-hex-8f2d56 hover:transition-colors
+          team-content
+          m-auto
+          w-100%
+          h-100%
+          bg-hex-640087
+          absolute
+          text-white
         "
       >
-        <img :src="imageUrl" alt="profile" class="w-100%" />
-      </div>
-      <div class="team-content m-auto">
-        <h3 class="title text-xl font-600 text-hex-8f2d56 pt-10 uppercase">
+        <h3 class="title text-lg font-700">
           {{ name }}
         </h3>
-        <span class="post text-hex-878484 font-500 text-lg"> {{ title }} </span>
-        <ul class="social m-0 p-2">
-          <li class="inline-block px-1">
+        <span class="post block text-md opacity-0 mb-5"> {{ title }} </span>
+        <ul class="icon">
+          <li class="inline-block">
             <a
               aria-label="linkedin"
               :href="socialMediaLink"
               class="
+                iconify
                 border-solid border
                 block
                 h-8
@@ -31,43 +42,38 @@
                 text-center
                 w-8
                 rounded-full
-                border-hex-8f2d56
-                bg-white
-                hover:bg-hex-8f2d56
+                hover:bg-hex-006192 hover:border-hex-006192
               "
             >
               <span
-                class="iconify text-hex-8f2d56 text-2xl m-auto"
+                class="iconify m-auto text-xl mt-1"
                 data-icon="uim:linkedin-alt"
               ></span>
             </a>
           </li>
-          <slot>
-            <li class="inline-block px-1">
-              <a
-                aria-label="github"
-                :href="githubLink"
-                class="
-                  border-solid border
-                  block
-                  h-8
-                  text-base
-                  leading-9
-                  text-center
-                  w-8
-                  rounded-full
-                  border-hex-8f2d56
-                  bg-white
-                  hover:bg-hex-8f2d56
-                "
-              >
-                <span
-                  class="iconify text-hex-8f2d56 text-2xl m-auto mt-0.5"
-                  :data-icon="icon"
-                ></span>
-              </a>
-            </li>
-          </slot>
+          <li class="inline-block">
+            <a
+              aria-label="github"
+              :href="githubLink"
+              class="
+                iconify
+                border-solid border
+                block
+                h-8
+                text-base
+                leading-9
+                text-center
+                w-8
+                rounded-full
+                hover:bg-gray-900 hover:border-gray-900
+              "
+            >
+              <span
+                class="iconify m-auto text-xl mt-1"
+                :data-icon="icon"
+              ></span>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
@@ -108,14 +114,51 @@ export default {
 </script>
 
 <style>
-.our-team .pic {
-  border-radius: 50% 50% 50% 0;
-  transition: all 0.5s ease 0s;
+.our-team {
+  padding: 20px 20px;
+  border-radius: 50%;
 }
-.our-team:hover .pic {
-  border-radius: 50% 0;
+.our-team img {
+  transform: scale(1);
+  transition: all 1s ease 0s;
 }
-.our-team:hover .social li a {
-  transition: all 0.5s ease 0s;
+.our-team:hover img {
+  transform: scale(1.2);
+}
+.our-team .team-content {
+  width: 100%;
+  height: 100%;
+  background: #640087;
+
+  padding-top: 35%;
+  color: #fff;
+  opacity: 0;
+  top: 0;
+  left: 0;
+  transition: all 0.5s ease-out 0s;
+}
+.our-team:hover .team-content {
+  opacity: 0.8;
+}
+.our-team .title {
+  transform: rotate(-90deg);
+  transition: all 0.3s ease-in-out 0.2s;
+}
+.our-team:hover .title {
+  opacity: 1;
+  transform: scale(1);
+  transition-delay: 0.1s;
+}
+.our-team .post {
+  transform: scale(1.2);
+  transition: all 0.3s ease-in-out 0.2s;
+}
+.our-team:hover .post {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.our-team .icon li a {
+  transition: all 0.3s ease 0s;
 }
 </style>
--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,6 +1,17 @@
 <template>
   <section class="overflow-hidden">
-    <div class="h-35vh relative z-1 bg-hex-1f2124 lg:text-xl w-100%">
+    <div
+      class="
+        h-auto
+        md:h-60vh
+        lg:h-50vh
+        relative
+        z-1
+        bg-hex-050505
+        lg:text-xl
+        w-100%
+      "
+    >
       <transition appear @before-enter="beforeEnter" @enter="enter">
         <h2
           class="
@@ -18,13 +29,12 @@
       <transition appear @before-enter="beforeEnter" @enter="enter">
         <p
           class="
-            w-80
-            md:w-200
-            lg:w-300
+            w-80vw
             m-auto
             text-gray-300
             font-400
             text-lg
+            mt-10
             md:text-xl
             sm:text-md
             text-center
@@ -38,9 +48,9 @@
       </transition>
       <client-only>
         <Particles
-          class="z-0 absolute w-100% inset-0 h-35vh"
+          class="z-0 absolute w-100% inset-0 h-50vh"
           color="#ccc"
-          :particles-number="100"
+          :particles-number="70"
           shape-type="circle"
           :particle-size="4"
           movement-direction="top-left"
@@ -50,89 +60,175 @@
         />
       </client-only>
     </div>
-    <transition appear @before-enter="beforeEnter" @enter="enter">
-      <div
-        class="
-          -mt-14
-          w-screen
-          md:w-200
-          lg:w-350 lg:p-20
-          drop-shadow-sm
-          z-1
-          relative
-        "
-      >
-        <div class="nav flex">
-          <a href="#mem" class="nav-elements w-1/2" @click="showAll()">
-            All Team
+    <main
+      class="w-100% lg:h-55vh bg-hex-101010 drop-shadow-sm z-1 relative p-10"
+    >
+      <div class="nav flex p-10">
+        <div class="nav-elements w-1/2 p-8 drop-shadow-3xl">
+          <lottie
+            :width="250"
+            :height="250"
+            :options="lottieOptions.first"
+            class="lottie"
+            @animCreated="handleAnimation($event, 'firstAnim')"
+          />
+          <span class="text-3xl mt-10 py-10">Developers</span>
+          <p class="text-sm mt-10">
+            lorem ipsum dolor sit amet, consectetur adipis, lorem ipsum dolor
+            sit.
+          </p>
+          <a href="#mem">
+            <button
+              class="
+                btn
+                w-30
+                p-2
+                mt-10
+                border
+                rounded-lg
+                hover:bg-hex-fff hover:text-black
+              "
+              @click="showSdg()"
+            >
+              View Team
+            </button>
           </a>
-          <a href="#mem" class="nav-elements w-1/2" @click="showSdg()">
-            Developers
-          </a>
-          <a href="#mem" class="nav-elements w-1/2" @click="showAlums()">
-            Alums
+        </div>
+        <div class="nav-elements w-50vw p-8 -mt-20">
+          <lottie
+            :width="250"
+            :height="250"
+            :options="lottieOptions.second"
+            class="lottie"
+            @mouseover="play"
+            @focus="play"
+            @animCreated="handleAnimation($event, 'secondAnim')"
+          />
+          <span class="text-3xl mt-10 py-10">Alums</span>
+          <p class="text-sm mt-10">
+            lorem ipsum dolor sit amet, consectetur adipis, lorem ipsum dolor
+            sit.
+          </p>
+          <a href="#mem">
+            <button
+              href="#mem"
+              class="
+                btn
+                w-30
+                p-2
+                mt-10
+                border
+                font-400
+                rounded-lg
+                hover:bg-hex-fff hover:text-black
+              "
+              @click="showAlums()"
+            >
+              View Team
+            </button>
           </a>
         </div>
       </div>
-    </transition>
-    <div class="text-center">
-      <h2
-        v-if="showCard && showAlum"
-        class="text-white text-center text-3xl md:text-4xl lg:text-6xl"
-      >
-        ALL TEAM
-      </h2>
-      <h2
-        v-if="showCard && !showAlum"
-        class="text-white text-center text-3xl md:text-4xl lg:text-6xl"
-      >
-        DEVELOPER TEAM
-      </h2>
-      <div v-for="member in members" id="mem" :key="member" class="m-auto">
-        <transition appear @before-enter="beforeEnter" @enter="enter">
-          <sdgCard
-            v-show="showCard"
-            :name="member.name"
-            :title="member.title"
-            :social-media-link="member.link"
-            :github-link="member.github"
-            :image-url="`${member.github}.png`"
-            class="w-120 md:w-220 lg:w-320 m-auto px-10 flex md:block lg:block"
-          />
-        </transition>
+      <div class="text-center p-20">
+        <h2
+          v-if="showCard && !showAlum"
+          class="
+            text-white text-center text-4xl
+            md:text-4xl
+            lg:text-6xl
+            m-auto
+            -mx-20
+          "
+        >
+          Our Team
+        </h2>
+        <div
+          v-for="member in members"
+          id="mem"
+          :key="member"
+          class="m-auto mt-20 -mx-35 md:-mx-20 lg:mx-auto"
+        >
+          <transition appear @before-enter="beforeEnter" @enter="enter">
+            <sdgCard
+              v-show="showCard"
+              :name="member.name"
+              :title="member.title"
+              :social-media-link="member.link"
+              :github-link="member.github"
+              :image-url="`${member.github}.png`"
+              class="
+                w-100
+                md:w-220
+                lg:w-320 lg:m-auto
+                flex
+                md:block
+                lg:block
+                mt-20
+              "
+            />
+          </transition>
+        </div>
+        <h2
+          v-if="showAlum && !showCard"
+          class="text-white text-center text-3xl md:text-4xl lg:text-6xl"
+        >
+          ALUMNI
+        </h2>
+        <div
+          v-for="alum in alums"
+          id="mem"
+          :key="alum"
+          class="m-auto mt-20 -mx-35 md:-mx-20 lg:mx-auto"
+        >
+          <transition appear @before-enter="beforeEnter" @enter="enter">
+            <sdgCard
+              v-show="showAlum"
+              :name="alum.name"
+              :title="alum.title"
+              :social-media-link="alum.link"
+              :github-link="alum.github"
+              :image-url="`${alum.github}.png`"
+              class="
+                w-100
+                md:w-220
+                lg:w-300
+                m-auto
+                flex
+                md:block
+                lg:block
+                mt-20
+              "
+            />
+          </transition>
+        </div>
       </div>
-      <h2
-        v-if="showAlum && !showCard"
-        class="text-white text-center text-3xl md:text-4xl lg:text-6xl"
-      >
-        ALUMNI
-      </h2>
-      <div v-for="alum in alums" id="mem" :key="alum" class="m-auto">
-        <transition appear @before-enter="beforeEnter" @enter="enter">
-          <sdgCard
-            v-show="showAlum"
-            :name="alum.name"
-            :title="alum.title"
-            :social-media-link="alum.link"
-            :github-link="alum.github"
-            :image-url="`${alum.github}.png`"
-            class="w-120 md:w-220 lg:w-320 m-auto px-10 flex md:block lg:block"
-          />
-        </transition>
-      </div>
-    </div>
+    </main>
   </section>
 </template>
 <script>
 import { defineComponent } from '@nuxtjs/composition-api';
+import lottie from 'vue-lottie/src/lottie.vue';
+import ScrollTrigger from 'gsap';
+import * as devAnimationData from '~/assets/55885-developer.json';
+import * as alumniAnimationData from '~/assets/30304-back-to-school.json';
 export default defineComponent({
-  components: {},
+  components: {
+    lottie,
+  },
   data() {
     return {
       members: 8,
       alums: 8,
       showCard: false,
       showAlum: false,
+      anim: {
+        firstAnim: null,
+        secondAnim: null,
+      },
+      lottieOptions: {
+        first: { animationData: devAnimationData.default },
+        second: { animationData: alumniAnimationData.default },
+      },
     };
   },
   mounted() {
@@ -142,7 +238,6 @@ export default defineComponent({
     });
     this.fetchMembers();
     this.fetchAlums();
-    this.showAll();
   },
   methods: {
     async fetchMembers() {
@@ -152,10 +247,6 @@ export default defineComponent({
     async fetchAlums() {
       const data = await this.$content('alums').fetch();
       this.alums = data;
-    },
-    showAll() {
-      this.showCard = true;
-      this.showAlum = true;
     },
     showSdg() {
       this.showCard = true;
@@ -177,6 +268,12 @@ export default defineComponent({
         ease: 'bounce.out',
       });
     },
+    handleAnimation(anim, type) {
+      this.anim[type] = anim;
+    },
+    play(type) {
+      this.anim[type].play();
+    },
   },
 });
 </script>
@@ -184,18 +281,33 @@ export default defineComponent({
 .nav {
   margin-top: 5px;
   margin-inline: auto;
-  /* width: 150px; */
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 .nav-elements {
-  text-align: center;
-  font-size: 25px;
-  font-family: 'Roboto', monospace;
+  margin: 0 auto;
   letter-spacing: 2px;
-  /* border-bottom: 2px solid rgb(130, 247, 247);
-  transition: 0.3s; */
-  color: rgb(130, 247, 247);
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+    rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+    rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  color: #fff;
+  // display: flex;
+  width: 500px;
 }
+.nav-elements:hover {
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+    rgba(0, 0, 0, 0.22) 0px 15px 12px;
+}
+.nav-elements .lottie {
+  float: left;
+  position: relative;
+  margin-right: 30px;
+}
+
 body {
   scroll-behavior: smooth;
+  background: #050505;
 }
 </style>
