9cdeb253a3e8130443272c2b8dc856f17e4cba64
garima <garima.singh.eee20@itbhu.ac.in>
2022-01-27 17:10:46 +0530

    aboutus_edits

--- components/about-us/aboutuscard.vue
+++ components/about-us/aboutuscard.vue
@@ -17,14 +17,14 @@
       <div class="text <lg:(w-3/4 mt-5 h-1/3 text-center) w-1/2">
         <h1 class="mb-5px font-medium text-2xl text-gray-50">WHAT IS SDG?</h1>
         <p class="text-gray-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tortor
-          eros, consequat sit amet euismod rutrum, ultricies ut dui. Donec et
-          dui non lorem tincidunt scelerisque ut eu lacus. Nullam a nunc in nisi
-          varius suscipit at vitae ante. Nulla magna ex, laoreet a molestie eu,
-          sagittis id augue. Sed molestie hendrerit ex eu blandit. Nam ut
-          imperdiet purus. Fusce pretium sollicitudin dapibus. Etiam gravida
-          fermentum orci semper luctus. Maecenas est arcu, sagittis non purus
-          vitae, blandit egestas nibh.
+          One of the prominent pillars of COPS, IITBHU is SDG. With the increasing 
+          percentage of the world's population possessing an innovative and feature
+          phone, the software has influenced many lives,  but ever considered how 
+          this software or the websites you surf day and night or any gaming application
+          you die for are developed? No worries, we at Software Developing Group are
+          here to unravel all these mysteries for you to perceive, prepare and produce.
+          We got you taken care of, from displaying Hello World on your Local Host 
+          to developing a fully functional mobile application.
         </p>
       </div>
       <div
@@ -88,14 +88,13 @@
       <div class="text <lg:(w-3/4 mt-5 h-1/3 text-center) w-1/2">
         <h1 class="mb-5px font-medium text-2xl text-gray-50">WHAT WE DO?</h1>
         <p class="text-gray-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tortor
-          eros, consequat sit amet euismod rutrum, ultricies ut dui. Donec et
-          dui non lorem tincidunt scelerisque ut eu lacus. Nullam a nunc in nisi
-          varius suscipit at vitae ante. Nulla magna ex, laoreet a molestie eu,
-          sagittis id augue. Sed molestie hendrerit ex eu blandit. Nam ut
-          imperdiet purus. Fusce pretium sollicitudin dapibus. Etiam gravida
-          fermentum orci semper luctus. Maecenas est arcu, sagittis non purus
-          vitae, blandit egestas nibh.
+          Whether Mobile Development, Desktop Development, Web Development, 
+          Game Development, or Cloud Development, we manufacture all units 
+          here! Along the lines of development, we enjoy discussions and 
+          collaborations too. Regular DevTalks for discussions and doubts 
+          about new tech and associating over Open Source provides us with 
+          great opportunities of expanding our colossal network. DevOps with GitLab, 
+          API with Postman, GSOC 101, and Android Study Jam are some of our sought-after workshops.
         </p>
       </div>
     </div>
@@ -125,14 +124,16 @@
           OUR AREAS OF INTEREST
         </h1>
         <p class="text-gray-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tortor
-          eros, consequat sit amet euismod rutrum, ultricies ut dui. Donec et
-          dui non lorem tincidunt scelerisque ut eu lacus. Nullam a nunc in nisi
-          varius suscipit at vitae ante. Nulla magna ex, laoreet a molestie eu,
-          sagittis id augue. Sed molestie hendrerit ex eu blandit. Nam ut
-          imperdiet purus. Fusce pretium sollicitudin dapibus. Etiam gravida
-          fermentum orci semper luctus. Maecenas est arcu, sagittis non purus
-          vitae, blandit egestas nibh.
+          Our aim is constantly learning and revamping. Hackalog, 
+          our very own portal for maintaining logs of our Hackathon, 
+          helps us archive hackathon details and leaderboards. Lite Hai, 
+          our institute's app, is every student's guide regarding 
+          various student bodies, fests, workshops; and it also comes 
+          with Google Maps for easy movement within the campus. Our 
+          official website for the Software Development Group is a 
+          storehouse for various software development blogs and our 
+          achievements and projects. Our insatiable appetite towards 
+          developing adores creating Telegram Bots would be fair to say.
         </p>
       </div>
       <div
