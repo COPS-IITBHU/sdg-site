b82132fec42c5d5a79aa09aa1009ad5dba6f6746
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-05-21 19:54:04 +0530

    refactor: smooth scroll to target elements
    scroll to teams for visibility
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -127,7 +127,7 @@
           </figcaption>
         </figure>
       </section>
-      <section v-if="showCard && !showAlum" id="team">
+      <section v-if="showCard && !showAlum" id="team-id">
         <div v-for="(value, header) in members" :key="header" class="text-center px-4 mx-auto my-4">
           <h2
             class="
@@ -161,7 +161,7 @@
       </section>
 
       <!-- Alums -->
-      <section v-if="showAlum && !showCard" id="alums" class="text-center p-20">
+      <section v-if="showAlum && !showCard" id="team-id" class="text-center p-20">
         <h2
           class="text-white text-center text-3xl md:text-4xl lg:text-6xl"
         >
@@ -265,15 +265,21 @@ export default defineComponent({
       this.showCard = true
       this.showAlum = false
       setTimeout(() => {
-        document.getElementById('team').scrollIntoView()
-      }, 100)
+        document.getElementById('team-id').scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
+      , 1000)
     },
     showAlums () {
       this.showAlum = true
       this.showCard = false
       setTimeout(() => {
-        document.getElementById('alums').scrollIntoView()
-      }, 100)
+        document.getElementById('team-id').scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
+      , 1000)
     },
     beforeEnter (el) {
       el.style.opacity = 0
