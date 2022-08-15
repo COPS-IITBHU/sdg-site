d862583b88deb4083aeac0a578b21e4c7fcd24e8
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-08-15 23:31:39 +0530

    feat: footer mt-auto main flex
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="p-4 bg-white rounded-lg shadow md:px-8 container mx-auto">
+  <footer class="p-4 bg-white rounded-lg shadow md:px-8 container mx-auto mt-auto">
     <hr class="my-2 border-gray-200 sm:mx-auto  lg:my-4">
     <div class="flex flex-col md:flex-row items-center md:justify-around">
       <a href="https://copsiitbhu.co.in" target="_blank" rel="noreferrer" class="flex items-center mb-4 sm:mb-0 order-1">
