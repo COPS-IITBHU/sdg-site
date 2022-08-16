5a2682d4de3c835e720a57beb263149380a473fa
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-08-16 18:37:10 +0530

    feat: new hero with shapes and text
    - gsap headline
    - sub head name
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,221 +1,93 @@
 <template>
-  <div>
-    <div class="flex justify-center h-0">
-      <section
-        class="
-          container
-          items-center
-          flex-col flex
-          relative
-          z-2
-          top-48
-          max-w-lg
-          md:max-w-2xl
-        "
-      >
+  <section class="mx-auto min-h-screen flex flex-col justify-evenly text-center relative">
+    <ul class="flex flex-col md:flex-row justify-evenly">
+      <li v-for="text in headingText" :key="text">
         <h1
-          data-text="SDG"
-          class="text-6xl sm:text-7xl md:text-9xl xl:text-11xl antialiased py-4 text-center"
+          class="text-5xl lg:text-7xl antialiased mx-2 my-2 text-center tracking-widest"
         >
-          SDG
+          {{ text }}
         </h1>
-        <p class="antialiased text-shadow-md text-center px-4 text-lg lg:text-xl">
-          <span class="font-bold text-xl md:text-3xl mb-8 font-display block">
-            Club Of ProgrammerS IIT (BHU), Varanasi</span
-          >
-          We at Software Development Group are a bunch of designers and developers who aim to
-          encourage the development of technology and innovation in IIT (BHU)
-          campus (and beyond) by learning, sharing knowledge and solving
-          problems.
-        </p>
-      </section>
-    </div>
-    <section id="box" class="flex flex-col overflow-hidden w-full h-screen">
-      <div
-        v-for="index in 27"
-        :key="index"
-        class="
-          row
-          transform
-          -rotate-30
-          relative
-          flex
-          p-0
-          whitespace-nowrap
-          text-6xl
-          -top-110
-        "
-      >
-        <div class="flex">
-          <Icon v-for="name in iconRow1" :key="name" :name="name" />
-        </div>
-        <div class="flex">
-          <Icon v-for="name in iconRow2" :key="name" :name="name" />
-        </div>
-      </div>
-    </section>
-  </div>
+      </li>
+    </ul>
+    <figure>
+      <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold">
+        Software Development Group
+      </h2>
+      <figcaption class="text-xl font-medium tracking-widest">Club of Programmers, IIT (BHU) Varanasi</figcaption>
+    </figure>
+    <p class="antialiased text-shadow-md px-4 text-lg lg:text-xl max-w-3xl mx-auto">
+      We at Software Development Group (SDG) are a bunch of software designers, architects and developers who aim to
+      encourage the development of technology and innovation in IIT (BHU)
+      campus (and beyond) by learning, sharing knowledge and solving
+      problems together.
+    </p>
+    <aside aria-hidden="true" class="absolute inset-0 overflow-hidden -z-10">
+      <img src="/shapes/dots.svg" alt="dots" class="ShapesBackground_shape" style="top: 40rem; left: 90vw; width: 10.5rem; height: 12.75rem; filter: var(--teal); opacity: 0.25;">
+      <img src="/shapes/dots.svg" alt="dots" class="ShapesBackground_shape" style="top: 40rem; right: 90vw; width: 10.5rem; height: 12.75rem; opacity: 0.25;">
+      <img src="/shapes/hash.svg" alt="hash" class="ShapesBackground_shape" style="top: 10rem; left: 9vw; width: 3.75rem; height: 3.75rem; filter: var(--teal);">
+      <img src="/shapes/hash.svg" alt="hash" class="ShapesBackground_shape" style="top: 10rem; right: 9vw; width: 3.75rem; height: 3.75rem;">
+      <img src="/shapes/triangle.svg" alt="triangle" class="hidden sm:block ShapesBackground_shape" style="top: 20rem; left: 24vw; width: 4.25rem; height: 4.25rem; transform: rotate(18deg); filter: var(--teal);">
+      <img src="/shapes/triangle.svg" alt="triangle" class="hidden sm:block ShapesBackground_shape" style="top: 20rem; right: 24vw; width: 4.25rem; height: 4.25rem; transform: rotate(-26deg);">
+      <img src="/shapes/waves.svg" alt="waves" class="ShapesBackground_shape" style="top: 30rem; left: 20vw; width: 7.25rem; height: 3.625rem;">
+      <img src="/shapes/waves.svg" alt="waves" class="ShapesBackground_shape" style="top: 30rem; right: 20vw; width: 6.375rem; height: 3.125rem; filter: var(--teal);">
+    </aside>
+  </section>
 </template>
-<script lang="ts">
+
+<script>
 import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   data () {
     return {
-      iconRow1: [
-        'bi:github',
-        'bx:bxl-apple',
-        'entypo:code',
-        'ion:logo-angular',
-        'dashicons:database',
-        'cib:octopus-deploy',
-        'fluent:branch-fork-24-filled',
-        'ion:logo-linkedin',
-        'akar-icons:react-fill',
-        'fa-brands:js-square',
-        'whh:ironman',
-        'la:node-js',
-        'fa-solid:robot',
-        'bx:bx-coffee-togo',
-        'bi:headset',
-        'teenyicons:npm-outline',
-        'ci:facebook',
-        'fluent:people-team-16-regular',
-        'fa-solid:quidditch',
-        'fluent:food-pizza-20-filled'
-      ],
-      iconRow2: [
-        'akar-icons:clock',
-        'ic:baseline-cloud-download',
-        'bi:camera-fill',
-        'uis:calender',
-        'la:reddit',
-        'bx:bxs-book-bookmark',
-        'ion:logo-python',
-        'la:github-alt',
-        'fluent:bluetooth-24-filled',
-        'fa-solid:user-secret',
-        'akar-icons:discord-fill',
-        'ic:outline-android',
-        'grommet-icons:technology',
-        'cib:google-chrome',
-        'bx:bxl-vuejs',
-        'bx:bxs-trash',
-        'grommet-icons:gamepad',
-        'bx:bxs-star',
-        'bx:bxl-telegram',
-        'ant-design:html5-filled',
-        'majesticons:ghost'
+      animation: null,
+      headingText: [
+        'Design',
+        'Develop',
+        'Deploy'
       ]
     }
+  },
+  mounted () {
+    const textShadowDepth = 8
+
+    const color = '#34b3c1'
+
+    let t = '0px 0px '
+    let TextShadowString = t + color
+    let fromTextShadowString = t + color
+
+    for (let i = 1; i <= textShadowDepth; i++) {
+      t = `, -${i}px ${i}px `
+      TextShadowString += t + color
+      fromTextShadowString += ', 0px 0px ' + color
+    }
+
+    this.animation = this.$gsap
+      .fromTo('h1', {
+        textShadow: fromTextShadowString
+      },
+      {
+        duration: 1.5,
+        stagger: 1.3,
+        delay: 1,
+        ease: 'sine',
+        repeat: -1,
+        yoyo: true,
+        yoyoEase: 'slow',
+        textShadow: TextShadowString
+      }
+      )
   }
 })
 </script>
-<style scoped>
 
-.row div {
-  animation: animate1 80s linear infinite;
-  animation-delay: -80s;
-}
-
-.row div:nth-child(2) {
-  animation: animate2 80s linear infinite;
-  animation-delay: -40s;
-}
-.row:nth-child(even) div {
-  animation: animate3 80s linear infinite;
-  animation-delay: -80s;
-}
-
-.row:nth-child(even) div:nth-child(2) {
-  animation: animate4 80s linear infinite;
-  animation-delay: -40s;
-}
-
-@keyframes animate1 {
-  0% {
-    transform: translateX(100%);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
-}
-
-@keyframes animate2 {
-  0% {
-    transform: translateX(0%);
-  }
-  100% {
-    transform: translateX(-200%);
-  }
-}
-
-@keyframes animate3 {
-  0% {
-    transform: translateX(-100%);
-  }
-  100% {
-    transform: translateX(100%);
-  }
-}
-
-@keyframes animate4 {
-  0% {
-    transform: translateX(-200%);
-  }
-  100% {
-    transform: translateX(0%);
-  }
-}
-
-.container h1 {
-  color: rgb(0, 238, 255);
-}
-.container p {
-  color: aliceblue;
-}
-
-.container h1::before {
-  position: absolute;
-  content: attr(data-text);
-  text-shadow: 0px 0px 20px rgb(0, 238, 255);
-  filter: blur(10px) brightness(0);
-  animation: flicker 5s linear infinite;
-  animation-delay: 1s;
-}
-@keyframes flicker {
-  0% {
-    filter: blur(5px) brightness(1);
-  }
-  3% {
-    filter: blur(5px) brightness(0);
-  }
-  6% {
-    filter: blur(5px) brightness(0);
-  }
-  7% {
-    filter: blur(5px) brightness(1);
-  }
-  8% {
-    filter: blur(5px) brightness(0);
-  }
-  9% {
-    filter: blur(5px) brightness(1);
-  }
-  10% {
-    filter: blur(5px) brightness(0);
-  }
-  20% {
-    filter: blur(5px) brightness(1);
-  }
-  50% {
-    filter: blur(5px) brightness(1);
-  }
-  99% {
-    filter: blur(5px) brightness(0);
-  }
-  100% {
-    filter: blur(5px) brightness(1);
-  }
+<style scoped lang="scss">
+.ShapesBackground_shape {
+    --blue: invert(53%) sepia(80%) saturate(4689%) hue-rotate(189deg) brightness(92%) contrast(93%);
+    --teal: invert(76%) sepia(39%) saturate(578%) hue-rotate(110deg) brightness(91%) contrast(88%);
+    position: absolute;
+    filter: var(--blue);
+    opacity: 0.2;
 }
 </style>
