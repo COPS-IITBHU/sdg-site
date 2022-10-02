32e9bbd43ce7d2771fc8988766a37e4cd823705d
Partik SIngh <partiksingh@pop-os.localdomain>
2022-10-03 01:03:18 +0530

    Changed hex colors with standarised colors

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -146,7 +146,6 @@
 import { defineComponent } from '@nuxtjs/composition-api'
 import Online from '~/components/contact-us/discord/online.vue'
 import Map from '~/components/contact-us/map.vue'
-
 interface itemsObject {
   name: string
   url: string
@@ -157,7 +156,6 @@ interface itemsObject {
     color: string
   }
 }
-
 export default defineComponent({
   components: { Map, Online },
   data () {
@@ -200,14 +198,13 @@ img {
 #content:hover img {
   transform: scale(1.1);
 }
-
 ul li a {
-  background-color: rgba(255, 255, 255, 0.911);
+  background-color: var(--contact-link-bg-primary);
   transition: 0.4s;
 }
 ul li a:hover {
   transform: translate(0, -5px) scale(1.05);
-  background-color: rgba(255, 255, 255, 0.979);
-  box-shadow: 0px 0px 18px rgba(116, 227, 235, 0.959);
+  background-color: var(--contact-link-bg-secondary);
+  box-shadow: 0px 0px 18px var(--contact-link-shadow-primary);
 }
 </style>
