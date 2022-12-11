8c0e920b4962fd7d3d25e9983237f011e1b4cb66
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-12-11 08:50:04 +0530

    Update social media icons

---
+++ components/contact-us/social.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="my-4 px-2">
+      <h3 class="py-2 text-cyan-400 text-3xl social-links">
+        We're also here....
+      </h3>
+      <br />
+      <p class="text-light-50 content">
+        Follow us on the following social media handles and stay apprised about
+        the latest events and workshops!
+      </p>
+      <div class="mx-4 my-8">
+        <ul class="flex flex-row flex-wrap justify-center items-center">
+          <li
+            v-for="item in items"
+            :key="item.name"
+            class="list-none mx-4 py-2"
+          >
+            <a
+              :href="item.url"
+              class="p-3 w-16 h-16 rounded-full flex items-center justify-center"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div class="icon-circle">
+                <span
+                  class="iconify"
+                  :data-icon="item.icon"
+                  :data-height="item.height"
+                  :data-width="item.width"
+                  style="color: black"
+                >
+                  {{ item.name }}
+                </span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import contactDetails from '@/content/contacts/socialMedia.json';
+
+export default defineComponent({
+  data() {
+    return {
+      items: contactDetails.items
+    };
+  }
+});
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Jura&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+
+.social-links {
+  color: #08fdd8;
+  font-family: 'Jura';
+}
+
+.content {
+  font-family: 'Nunito';
+}
+.icon-circle {
+  border-radius: 50%;
+  padding: 10px;
+  background: #fff;
+  border: 3px solid #000;
+  color: #000;
+  text-align: center;
+}
+</style>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,22 +2,17 @@
   <main class="text-center text-light-50">
     <div id="head-map" class="flex justify-center items-center">
       <Map class="w-full" />
-      <span class="map-title rounded-full absolute text-light-50 font-bold font-display
-      text-8xl p-4
-      <lg:(text-6xl p-4)
-      <md:(text-4xl p-4)
-      ">Contact Us</span>
+      <span
+        class="map-title rounded-full absolute text-light-50 font-bold font-display text-8xl p-4 <lg:(text-6xl p-4) <md:(text-4xl p-4)"
+      >Contact Us</span
+      >
     </div>
 
-    <div
-      class="flex flex-row justify-around m-6 mt-12 <lg:flex-col"
-    >
+    <div class="flex flex-row justify-around m-6 mt-12 <lg:flex-col">
       <div class="flex flex-col">
         <div class="text-left mx-auto mb-4 <lg:(w-full)">
           <div>
-            <h2 class="py-2 text-cyan-400 text-4xl font-bold">
-              LET'S TALK
-            </h2>
+            <h2 class="py-2 text-cyan-400 text-4xl font-bold">LET'S TALK</h2>
             <p class="text-light-50 text-justify">
               With over 1700 members, the Discord server is a haven for
               programming enthusiasts! <br />
@@ -29,29 +24,20 @@
           </div>
         </div>
         <div
-          class="
-            flex flex-row
-            justify-center
-            items-center
-            flex-wrap
-            <lg:(w-full)
-          "
+          class="flex flex-row justify-center items-center flex-wrap <lg:(w-full)"
         >
-          <Online class="w-full md:w-3/4 md:mr-auto ml-auto md:ml-0" :serverid="serverid" />
+          <Online
+            class="w-full md:w-3/4 md:mr-auto ml-auto md:ml-0"
+            :serverid="serverid"
+          />
         </div>
       </div>
 
       <div
         id="clg_img"
-        class="lg:ml-12 my-8 md:my-4 flex justify-center items-center">
-        <div
-          id="content"
-          class="
-            w-full
-            relative
-            overflow-hidden
-          "
-        >
+        class="lg:ml-12 my-8 md:my-4 flex justify-center items-center"
+      >
+        <div id="content" class="w-full relative overflow-hidden">
           <nuxt-img
             src="contact/iit-varanasi.webp"
             width="600"
@@ -60,19 +46,7 @@
             loading="lazy"
           />
           <div
-            class="
-              absolute
-              top-0
-              left-0
-              right-0
-              bottom-0
-              w-full
-              h-full
-              object-cover
-              flex flex-col
-              justify-center
-              items-center
-            "
+            class="absolute top-0 left-0 right-0 bottom-0 w-full h-full object-cover flex flex-col justify-center items-center"
           >
             <div class="w-3/5 flex flex-col justify-center items-center">
               <div class="box">
@@ -87,7 +61,9 @@
                   ></span>
                 </div>
                 <div>
-                  <h2 class="text-2xl lg:text-3xl font-bold text-light-50">VISIT US</h2>
+                  <h2 class="text-2xl lg:text-3xl font-bold text-light-50">
+                    VISIT US
+                  </h2>
                   <p class="text-light-50">
                     A Malaiyo date discussing some bugs sounds cool, right?
                     Visit us in our Natural Banarasi Habitat and let us treat
@@ -100,80 +76,25 @@
         </div>
       </div>
     </div>
-    <div class="my-4 px-2">
-      <h3 class="py-2 text-cyan-400 text-3xl font-bold">
-        WE'RE ALSO HERE
-      </h3>
-      <p class="text-light-50">
-        Follow us on the following social media handles and stay apprised about
-        the latest events and workshops!
-      </p>
-      <div class="mx-4 my-8">
-        <ul class="flex flex-row flex-wrap justify-center items-center">
-          <li
-            v-for="item in items"
-            :key="item.name"
-            class="list-none mx-4 py-2"
-          >
-            <a
-              :href="item.url"
-              class="
-            p-3
-            w-16
-            h-16
-            rounded-full
-            flex
-            items-center
-            justify-center
-          "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span
-                class="iconify"
-                :data-icon="item.icon"
-                :style="item.style"
-                :data-height="item.height"
-                :data-width="item.width"
-              >
-                {{ item.name }}
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <br />
+    <Social />
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
-import Online from '~/components/contact-us/discord/online.vue'
-import Map from '~/components/contact-us/map.vue'
-interface itemsObject {
-  name: string
-  url: string
-  icon: string
-  height: Number
-  width: Number
-  style: {
-    color: string
-  }
-}
+import { defineComponent } from '@nuxtjs/composition-api';
+import Online from '~/components/contact-us/discord/online.vue';
+import Map from '~/components/contact-us/map.vue';
+import Social from '~/components/contact-us/social.vue';
+
 export default defineComponent({
-  components: { Map, Online },
-  data () {
+  components: { Map, Online, Social },
+  data() {
     return {
-      items: [],
       serverid: '786971759496790046'
     } as {
-      items: itemsObject[],
-      serverid: string
-    }
-  },
-  async fetch () {
-    const data = await this.$content('contacts').fetch()
-    this.items = (data as unknown) as itemsObject[]
+      serverid: string;
+    };
   },
   head: {
     title: 'Contact',
@@ -186,7 +107,7 @@ export default defineComponent({
       }
     ]
   }
-})
+});
 </script>
 
 <style scoped>
@@ -211,7 +132,7 @@ ul li a:hover {
   background-color: var(--contact-link-bg-secondary);
   box-shadow: 0px 0px 18px var(--contact-link-shadow-primary);
 }
-.map-title{
+.map-title {
   background: rgba(0, 0, 0, 0.6);
 }
 </style>
